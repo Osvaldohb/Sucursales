44fb2a7b7fd366d5467be174e572e6a7604efb3a
Osvaldohb <osvaldonuevo@gmail.com>
2022-03-21 13:35:27 -0600

    Se termina la funcion promedio

--- main.cpp
+++ main.cpp
@@ -4,12 +4,13 @@
 using namespace std;
 void entrada(float Monto[' '][' '],int m,int n,int col, int fila);
 float sucursalM(float Monto[' '][' '],int m,int n,int col,int fila);
-float promedio(float Monto[' '][' '],int m,int n,int col,int fila);
+float promedio(float Monto[' '][' '],int m,int n,int col,int fila,int dec);
 
  main()
 {
     float Monto[' '][' '];
     int m,n;
+    int dec;
     
     cout<<"Bienvenido al calculo de empresas y sucursales"<<endl;
     cout<<"Ingresa los anios"<<endl;
@@ -19,7 +20,7 @@ float promedio(float Monto[' '][' '],int m,int n,int col,int fila);
     
     entrada(Monto,m,n,10,3);
     cout<<"La sucursal que mas vendio fue la numero: "<<sucursalM(Monto,m,n,10,3)<<endl;
-    promedio(Monto,m,n,10,3);
+    promedio(Monto,m,n,10,3,dec);
     
     getch();
 }
@@ -51,20 +52,26 @@ float sucursalM(float Monto[' '][' '],int m,int n,int col,int fila)
     return fila+1;
 }
 
-float promedio(float Monto[' '][' '],int m,int n,int col,int fila)
+float promedio(float Monto[' '][' '],int m,int n,int col,int fila,int dec)
 {
      int x,y;
-     float suma,Prom=0;
+     float suma,Prom=0,Max=0;
     for(x=0;x<n;x++){
         suma=0;
         for(y=0;y<m;y++)
            {
-            suma+=Monto[x][y];    
+            suma+=Monto[y][x];  
         }
         Prom=suma/m;
+        if(Max<Prom)
+                {
+                    Max=Prom;
+                    col=x;
+                     }
         
         cout<<"El promedio de ventas del anio: "<<x+1<<" Es de: "<<Prom<<endl;
         
     }
+    cout<<"El anio con mas promedio de ventas es el anio: "<<col+1<<" Con un promedio de: "<<Max<<endl;
    
 }

15b47a0b2574f1d4411b72504dc7b3245ff5a409
Osvaldohb <osvaldonuevo@gmail.com>
2022-03-21 14:01:49 -0600

    Se termina el proyecto

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <conio.h>
+ #include <stdlib.h>
+ #include <windows.h>
 
 using namespace std;
 void entrada(float Monto[' '][' '],int m,int n,int col, int fila);
@@ -9,7 +11,7 @@ float promedio(float Monto[' '][' '],int m,int n,int col,int fila,int dec);
  main()
 {
     float Monto[' '][' '];
-    int m,n;
+    int m,n,sm;
     int dec;
     
     cout<<"Bienvenido al calculo de empresas y sucursales"<<endl;
@@ -17,11 +19,27 @@ float promedio(float Monto[' '][' '],int m,int n,int col,int fila,int dec);
     cin>>m;
     cout<<"Ingresa las sucursales"<<endl;
     cin>>n;
-    
     entrada(Monto,m,n,10,3);
-    cout<<"La sucursal que mas vendio fue la numero: "<<sucursalM(Monto,m,n,10,3)<<endl;
-    promedio(Monto,m,n,10,3,dec);
-    
+    sm=sucursalM(Monto,m,n,10,3);
+    system("cls");
+    cout<<"Porfavor eliga la opcion que corresponda a su busqueda"<<endl;
+    cout<<"1.-Sucursal que mas a vendido en "<<m<<" anios"<<endl;
+    cout<<"2.-Promedio de ventas por anio"<<endl;
+    cout<<"3.-Anio con mayor promedio de ventas."<<endl;
+    cin>>dec;
+    system("cls");
+    if(dec==1)
+    { 
+        cout<<"La sucursal que mas vendio fue la numero: "<<sm<<endl;
+    }if(dec==2)
+    {
+         promedio(Monto,m,n,10,3,dec);
+    }
+    if(dec==3)
+    {
+        promedio(Monto,m,n,10,3,dec);
+    }
+     
     getch();
 }
 
@@ -68,10 +86,14 @@ float promedio(float Monto[' '][' '],int m,int n,int col,int fila,int dec)
                     Max=Prom;
                     col=x;
                      }
-        
+        if(dec==2)
+        {
         cout<<"El promedio de ventas del anio: "<<x+1<<" Es de: "<<Prom<<endl;
+        }
         
     }
+    if(dec==3)
+    {
     cout<<"El anio con mas promedio de ventas es el anio: "<<col+1<<" Con un promedio de: "<<Max<<endl;
-   
+    }
 }
